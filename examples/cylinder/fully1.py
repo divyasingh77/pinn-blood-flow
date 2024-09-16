ed76cfd55a9cd82e2a3e5819e1f3ae6986e2617f
divyasingh77 <p23ma0006@iitj.ac.in>
2024-09-16 14:17:10 +0530

    fully1.py

--- examples/cylinder/fully1.py
+++ examples/cylinder/fully1.py
@@ -13,7 +13,6 @@ from modulus.sym.domain.constraint import (
     PointwiseBoundaryConstraint,
     PointwiseInteriorConstraint,
     IntegralBoundaryConstraint,
-    PointwiseConstraint,
 )
 from modulus.sym.domain.validator import PointwiseValidator
 from modulus.sym.domain.monitor import PointwiseMonitor
@@ -33,22 +32,35 @@ import pandas as pd
 import sympy
 
 
-@modulus.sym.main(config_path="conf", config_name="conf")
+@modulus.main(config_path="conf", config_name="conf")
 def run(cfg: ModulusConfig) -> None:
-  
+    print(to_yaml(cfg))
 
     # path definitions
     point_path = to_absolute_path("./stl_files")
     path_inlet = point_path + "/inlet.stl"
-    path_outlet = point_path + "/outlet.stl"
+    dict_path_outlet = {'path_outlet': point_path + "/outlet.stl",
+                        }
     path_noslip = point_path + "/wall.stl"
     path_interior = point_path + "/closed.stl"
+    path_outlet_combined = point_path + '/outlet_combined.stl'
+
+    # create and save combined outlet stl
+    def combined_stl(meshes, save_path="./combined.stl"):
+        combined = mesh.Mesh(np.concatenate([m.data for m in meshes]))
+        combined.save(save_path, mode=stl.Mode.ASCII)
+
+    meshes = [mesh.Mesh.from_file(file_) for file_ in dict_path_outlet.values()]
+    combined_stl(meshes, path_outlet_combined)
 
     # read stl files to make geometry
     inlet_mesh = Tessellation.from_stl(path_inlet, airtight=True)
-    outlet_mesh  = Tessellation.from_stl(path_outlet, airtight=True)
+    dict_outlet = {}
+    for idx_, key_ in enumerate(dict_path_outlet):
+        dict_outlet['outlet'+str(idx_)+'_mesh'] = Tessellation.from_stl(dict_path_outlet[key_], airtight=True)
     noslip_mesh = Tessellation.from_stl(path_noslip, airtight=True)
     interior_mesh = Tessellation.from_stl(path_interior, airtight=True)
+    outlet_combined_mesh = Tessellation.from_stl(path_outlet_combined, airtight=True)
 
     # params
     # blood density
@@ -60,20 +72,10 @@ def run(cfg: ModulusConfig) -> None:
     # velocity in center of inlet (parabolic profile)
     inlet_vel = 0.3
 
-    # inlet velocity profile
-    def circular_parabola(x, y, z, center, normal, radius, max_vel):
-        centered_x = x - center[0]
-        centered_y = y - center[1]
-        centered_z = z - center[2]
-        distance = sqrt(centered_x ** 2 + centered_y ** 2 + centered_z ** 2)
-        parabola = max_vel * Max((1 - (distance / radius) ** 2), 0)
-        return normal[0] * parabola, normal[1] * parabola, normal[2] * parabola
-
     # normalize meshes
     def normalize_mesh(mesh, center, scale):
-        mesh = mesh.translate([-c for c in center])
-        mesh = mesh.scale(scale)
-        return mesh
+        mesh.translate([-c for c in center])
+        mesh.scale(scale)
 
     # normalize invars
     def normalize_invar(invar, center, scale, dims=2):
@@ -87,28 +89,55 @@ def run(cfg: ModulusConfig) -> None:
             invar["area"] *= scale ** dims
         return invar
 
-    # geometry scaling and center of overall geometry
+    # geometry scaling
     scale = 1  # turn off scaling
+
+    # center of overall geometry
     center = (2.5, 0, 0)
     print('Overall geometry center: ', center)
 
     # scale and center the geometry files
-    inlet_mesh = normalize_mesh(inlet_mesh, center, scale)
-    outlet_mesh = normalize_mesh(outlet_mesh, center, scale)
-    noslip_mesh = normalize_mesh(noslip_mesh, center, scale)
-    interior_mesh = normalize_mesh(interior_mesh, center, scale)
+    normalize_mesh(inlet_mesh, center, scale)
+    for idx_, key_ in enumerate(dict_outlet):
+        normalize_mesh(dict_outlet[key_], center, scale)
+    normalize_mesh(noslip_mesh, center, scale)
+    normalize_mesh(interior_mesh, center, scale)
 
     # center of inlet in original coordinate system
+    inlet_center_abs = (0, 0, 0)
+    print("inlet_center_abs:", inlet_center_abs)
+
+    # scale end center the inlet center
+    inlet_center = list((np.array(inlet_center_abs) - np.array(center)) * scale)
+    print("inlet_center:", inlet_center)
+
+    # inlet normal vector; should point into the cylinder, not outwards
     inlet_normal = (1, 0, 0)
+    print("inlet_normal:", inlet_normal)
+
+    # inlet area
     inlet_area = (0.5**2 * np.pi) * (scale**2)
-    inlet_center = (-2.5, 0, 0)
+
+    # inlet radius
     inlet_radius = 0.5
+    print("inlet_radius:", inlet_radius)
     
     # Volumetric flow (= to mass flow for incompressible fluid) at inlet
     # (2*pi*vmax) * integrate{r*(1-r^2/R^2) dr from r=0 to R}
     inlet_volumetric_flow = inlet_vel/2. * inlet_area
     print("Volumetric flow at inlet:", inlet_volumetric_flow)
 
+    # inlet velocity profile
+    def circular_parabola(x, y, z, center, normal, radius, max_vel):
+        centered_x = x - center[0]
+        centered_y = y - center[1]
+        centered_z = z - center[2]
+
+        distance = sqrt(centered_x ** 2 + centered_y ** 2 + centered_z ** 2)
+        parabola = max_vel * Max((1 - (distance / radius) ** 2), 0)
+        return normal[0] * parabola, normal[1] * parabola, normal[2] * parabola
+
+    # make aneurysm domain
     domain = Domain()
 
     # make list of nodes to unroll graph on
@@ -119,12 +148,12 @@ def run(cfg: ModulusConfig) -> None:
         output_keys=[Key("u"), Key("v"), Key("w"), Key("p")],
         cfg=cfg.arch.fully_connected,
         layer_size=256,
-        nr_layers=2
+        nr_layers=10
     )
     nodes = (
             ns.make_nodes()
             + normal_dot_vel.make_nodes()
-            + [flow_net.make_node(name="flow_network")]
+            + [flow_net.make_node(name="flow_network", jit=cfg.jit)]
     )
 
     # add constraints to solver
@@ -147,14 +176,15 @@ def run(cfg: ModulusConfig) -> None:
     domain.add_constraint(inlet, "inlet")
 
     # outlet
-    outlet = PointwiseBoundaryConstraint(
-        nodes=nodes,
-        geometry=outlet_mesh,
-        outvar={"p": 0},
-        batch_size=cfg.batch_size.outlet,
-    )
-    domain.add_constraint(outlet, "outlet")
-                                                            
+    for idx_, key_ in enumerate(dict_outlet):
+        outlet = PointwiseBoundaryConstraint(
+            nodes=nodes,
+            geometry=dict_outlet[key_],
+            outvar={"p": 0},
+            batch_size=cfg.batch_size.outlet,
+        )
+        domain.add_constraint(outlet, "outlet"+str(idx_))
+
     # no slip
     no_slip = PointwiseBoundaryConstraint(
         nodes=nodes,
@@ -176,7 +206,7 @@ def run(cfg: ModulusConfig) -> None:
     # Integral Continuity 1
     integral_continuity = IntegralBoundaryConstraint(
         nodes=nodes,
-        geometry=outlet_mesh,
+        geometry=dict_outlet['outlet0_mesh'],
         outvar={"normal_dot_vel": 0.11780972450961724 * scale**2}, # (2*pi*vmax) * integrate{r*(1-r^2/R^2) dr from r=0 to R} 
         batch_size=cfg.batch_size.num_integral_continuity,
         integral_batch_size=cfg.batch_size.integral_continuity,
@@ -184,6 +214,50 @@ def run(cfg: ModulusConfig) -> None:
         lambda_weighting={"normal_dot_vel": 0.1},
     )
     domain.add_constraint(integral_continuity, "integral_continuity_1")
+
+    # add inferencer
+    inferencer_derivatives_boundary = PointwiseInferencer(
+        noslip_mesh.sample_boundary(250000),
+        ["p", "u", "v", "w",
+         "u__x", "u__y", "u__z",
+         "v__x", "v__y", "v__z",
+         "w__x", "w__y", "w__z",
+         "p__x", "p__y", "p__z",
+         "u__x__x", "u__y__y", "u__z__z",
+         "v__x__x", "v__y__y", "v__z__z",
+         "w__x__x", "w__y__y", "w__z__z",
+         "p__x__x", "p__y__y", "p__z__z",
+         "u__x__y", "u__x__z", "u__y__z",
+         "v__x__y", "v__x__z", "v__y__z",
+         "w__x__y", "w__x__z", "w__y__z",
+         "p__x__y", "p__x__z", "p__y__z",
+         "normal_x", "normal_y", "normal_z", "normal_dot_vel"
+         ],
+        nodes=nodes,
+    )
+    domain.add_inferencer(inferencer_derivatives_boundary, "inferencer_derivatives_boundary")
+
+    # add inferencer
+    inferencer_derivatives_interior = PointwiseInferencer(
+        noslip_mesh.sample_interior(250000),
+        ["p", "u", "v", "w",
+         "u__x", "u__y", "u__z",
+         "v__x", "v__y", "v__z",
+         "w__x", "w__y", "w__z",
+         "p__x", "p__y", "p__z",
+         "u__x__x", "u__y__y", "u__z__z",
+         "v__x__x", "v__y__y", "v__z__z",
+         "w__x__x", "w__y__y", "w__z__z",
+         "p__x__x", "p__y__y", "p__z__z",
+         "u__x__y", "u__x__z", "u__y__z",
+         "v__x__y", "v__x__z", "v__y__z",
+         "w__x__y", "w__x__z", "w__y__z",
+         "p__x__y", "p__x__z", "p__y__z",
+         "sdf__x", "sdf__y", "sdf__z", "sdf"
+         ],
+        nodes=nodes,
+    )
+    domain.add_inferencer(inferencer_derivatives_interior, "inferencer_derivatives_interior")
 
     # add validation data (CFD data from MODSIM)
     mapping = {
@@ -194,37 +268,22 @@ def run(cfg: ModulusConfig) -> None:
         "U:1": "v",
         "U:2": "w",
         "p": "p",
+        "wss": "u__y"
     }
     #modsim entire geometry
     modsim_var = csv_to_dict(to_absolute_path("modsim/modsim_wfenz_hiresbl.csv"), mapping)
-    modsim_invar = {
-        key: value for key, value in modsim_var.items() if key in ["x", "y", "z"]
-    }
+    modsim_invar = {key: value for key, value in modsim_var.items() if key in ["x", "y", "z"]}
     modsim_invar = normalize_invar(modsim_invar, center, scale, dims=3)
-    modsim_outvar = {
-        key: value 
-        for key, value in modsim_var.items()
-        if key in ["u", "v", "w", "p"]
-    }
-    modsim_validator = PointwiseValidator(
-        modsim_invar, modsim_outvar, nodes, batch_size=4096,
-    )
+    modsim_outvar = {key: value for key, value in modsim_var.items() if key in ["u", "v", "w", "p", "u__y"]}
+    modsim_validator = PointwiseValidator(modsim_invar, modsim_outvar, nodes, batch_size=4096)
     domain.add_validator(modsim_validator, "modsim_hiresbl_validator")
 
     #modsim plane slice
     modsim_var = csv_to_dict(to_absolute_path("modsim/modsim_plane_slice.csv"), mapping)
-    modsim_invar = {
-        key: value for key, value in modsim_var.items() if key in ["x", "y", "z"]
-    }
+    modsim_invar = {key: value for key, value in modsim_var.items() if key in ["x", "y", "z"]}
     modsim_invar = normalize_invar(modsim_invar, center, scale, dims=3)
-    modsim_outvar = {
-        key: value 
-        for key, value in modsim_var.items()
-        if key in ["u", "v", "w", "p"]
-    }
-    modsim_validator = PointwiseValidator(
-        modsim_invar, modsim_outvar, nodes, batch_size=4096,
-    )
+    modsim_outvar = {key: value for key, value in modsim_var.items() if key in ["u", "v", "w", "p", "u__y"]}
+    modsim_validator = PointwiseValidator(modsim_invar, modsim_outvar, nodes, batch_size=4096)
     domain.add_validator(modsim_validator, "modsim_plane_slice_validator")
 
     # add pressure monitor
@@ -238,7 +297,7 @@ def run(cfg: ModulusConfig) -> None:
 
     # add pressure monitor
     pressure_outlet = PointwiseMonitor(
-        outlet_mesh.sample_boundary(2048),
+        dict_outlet['outlet0_mesh'].sample_boundary(2048),
         output_names=["p"],
         metrics={"pressure_outlet": lambda var: torch.mean(var["p"])},
         nodes=nodes,
@@ -254,7 +313,7 @@ def run(cfg: ModulusConfig) -> None:
     domain.add_monitor(umax_inlet)
 
     umax_outlet = PointwiseMonitor(
-        outlet_mesh.sample_boundary(4096),
+        dict_outlet['outlet0_mesh'].sample_boundary(4096),
         output_names=["u"],
         metrics={"umax_outlet": lambda var: torch.max(var["u"])},
         nodes=nodes,
